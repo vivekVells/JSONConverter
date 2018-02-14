be159a4a7e90ad349f89a7160c4e0a3dc63a8d56
Vivek Vellaiyappan <techengineervivek@gmail.com>
2018-02-14 01:48:44 -0500

    fixed duplicate key issue. hashmap wont allow duplicate keys. Unable to map a single key to multiple values

--- src/jsonconverter/JsonCMain.java
+++ src/jsonconverter/JsonCMain.java
@@ -6,10 +6,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
+
+import javafx.util.Pair;
 
 /*
  * Logic:
@@ -20,7 +23,7 @@ import com.google.gson.Gson;
 
 public class JsonCMain {
 	
-	//private static Scanner scan = new Scanner(System.in);
+	private static Scanner scan = new Scanner(System.in);
 	protected String keyValuePairInput;
 	protected ArrayList<String> keyValueList = new ArrayList<>();
 	protected HashMap<String, String> keyValueMap = new HashMap<>();
@@ -38,9 +41,9 @@ public class JsonCMain {
 		System.out.println("Today: " + utility.Utility.getCurrentDateTime() + "\n");		
 	}
 	
-	protected void getJsonFile(HashMap<String, String> keyValueMap) {
-		String json = gsonObj.toJson(keyValueMap);
-		System.out.println("\nJSON String: " + json);
+	protected void getJsonFile(List<Pair<String, String>> dupKeyValList) {
+		String json = gsonObj.toJson(dupKeyValList);
+		System.out.println("\nJSON String: \n" + utility.Utility.getPrettyPrintJson(json));
 		
 		// Have to resolve the platform dependent path issue
 		String filePath =  "src\\resources\\files\\";
@@ -56,25 +59,37 @@ public class JsonCMain {
 		}
 		
 	}
-	
-	protected void getKeyValuePairs(ArrayList<String> keyValueList) {
-		System.out.println("Input: " + keyValueList);
-		for (int index = 0; index < keyValueList.size(); index++) {
-			this.keyValueMap.put(keyValueList.get(index), keyValueList.get(++index));
-		}
-		
+
+	protected void getKeyValueOut(HashMap<String, String> keyValueMap)  {
 		System.out.println("\nKey:Value mapping");
 		for (Entry<String, String> kv: this.keyValueMap.entrySet()) {
 			System.out.println("key: " + kv.getKey() + " || value: " + kv.getValue());
+		}		
+	}
+	
+	protected void getKeyValuePairs(ArrayList<String> keyValueList) {		
+		List<Pair<String, String>> dupKeyValList = new ArrayList<Pair<String, String>>();
+		
+		System.out.println("Input: " + keyValueList);
+		
+		for (int index = 0; index < keyValueList.size(); index++) {
+			dupKeyValList.add(new Pair<String, String>(keyValueList.get(index), keyValueList.get(++index)));
 		}
-		getJsonFile(keyValueMap);
+		getJsonFile(dupKeyValList);
+		
+	
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
+		System.out.println(dupKeyValList.toString());
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&");
 	}
 	
 	protected void getInput() {
 		System.out.println("Input the following appropriately.\nUse key,value combination..\n\n");
 		System.out.println("You can start from below:\n");
-		this.keyValuePairInput = "k1,v1,2,v2,k3,v3,@,#,$,%,^,&,*,~";
+		//this.keyValuePairInput = "k1,v1,2,v2,k3,v3,@,#,$,%,^,&,*,~";
+		this.keyValuePairInput = "a,s,n,m,a,z,a,q,s,w,s,x,d,e,d,x,d,f";
 		this.keyValueList = new ArrayList<>(Arrays.asList(this.keyValuePairInput.split(",")));
+		// a,s,n,m,a,z,a,q,s,w,s,x,d,e,d,x,d,f,f,d,f,g,f,v,g,f,g,b,g,t,h,j,h,n,h,y,h,u,j,k,j,m,j,i,k,i,k,m,l,o,l,;,l
 		getKeyValuePairs(keyValueList);
 	}
 	
@@ -87,31 +102,27 @@ public class JsonCMain {
 }
 
 /*
- *  1st version Output: (NOT BAD)
-		********************************************************************************************************************************************************
-																													JSON CONVERTER PAGE
-		********************************************************************************************************************************************************
-		Today: 2018/02/12 22:02:50
-		
-		Input the following appropriately.
-		Use key,value combination..
-		
-		
-		You can start from below:
-		
-		Input: [k1, v1, 2, v2, k3, v3, @, #, $, %, ^, &, *, ~]
-		
-		Key:Value mapping
-		key: @ || value: #
-		key: 2 || value: v2
-		key: $ || value: %
-		key: k1 || value: v1
-		key: k3 || value: v3
-		key: * || value: ~
-		key: ^ || value: &
-		
-		JSON String: {"@":"#","2":"v2","$":"%","k1":"v1","k3":"v3","*":"~","^":"\u0026"}
-		
-		File: json.json was successfully exported. 
-		Location: src\resources\files\json.json
+ 2nd Version output:
+ 
+********************************************************************************************************************************************************
+JSON CONVERTER PAGE
+********************************************************************************************************************************************************
+Today: 2018/02/14 01:02:26
+
+Input the following appropriately.
+Use key,value combination..
+
+
+You can start from below:
+
+Input: [a, s, n, m, a, z, a, q, s, w, s, x, d, e, d, x, d, f]
+
+JSON String: 
+"[{\"key\":\"a\",\"value\":\"s\"},{\"key\":\"n\",\"value\":\"m\"},{\"key\":\"a\",\"value\":\"z\"},{\"key\":\"a\",\"value\":\"q\"},{\"key\":\"s\",\"value\":\"w\"},{\"key\":\"s\",\"value\":\"x\"},{\"key\":\"d\",\"value\":\"e\"},{\"key\":\"d\",\"value\":\"x\"},{\"key\":\"d\",\"value\":\"f\"}]"
+
+File: json.json was successfully exported. 
+Location: src\resources\files\json.json
+&&&&&&&&&&&&&&&&&&&&&&&&
+[a=s, n=m, a=z, a=q, s=w, s=x, d=e, d=x, d=f]
+&&&&&&&&&&&&&&&&&&&&&&&&
 */
